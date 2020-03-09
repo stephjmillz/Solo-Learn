e4a63cc6ea91afbfb9294db3bf3f547f6722a967
Stephjmillz <mills.stephon@yahoo.com>
2020-03-09 13:24:48 -0700

    another commit

---
+++ src/main/java/Top5.java
@@ -0,0 +1,9 @@
+public class Top5 {
+    public static void main(String[] args) {
+        int x = 10;
+        double p = 23.0;
+        float y = 2.3f;
+        float answer = x * y;
+        System.out.println(answer);
+    }
+}
